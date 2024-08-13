0696e6d8650e514edc9b285fb815cf9a63fd80ea
Tarynleewalbrugh25 <146818417+Tarynleewalbrugh25@users.noreply.github.com>
2024-08-13 13:11:15 +0200

    Update ErrorHandling.js

--- Middleware/ErrorHandling.js
+++ Middleware/ErrorHandling.js
@@ -2,7 +2,7 @@ function errorHandling(err, req, res, next){
     if(err || res.statusCode >= 400){
         res.json({
             status: err.status || res.statusCode || 500,
-            msg: "An error occurred. Please try again later"
+            msg: err.message
         })
     }else {
         next()
