2a0288a6b45c2142b1361517b1f34e6f453550c2
Tarynleewalbrugh25 <walbrught@gmail.com>
2024-08-13 15:05:56 +0200

    up

--- Middleware/ErrorHandling.js
+++ Middleware/ErrorHandling.js
@@ -2,7 +2,7 @@ function errorHandling(err, req, res, next){
     if(err || res.statusCode >= 400){
         res.json({
             status: err.status || res.statusCode || 500,
-            msg: 'An error has occurred.'
+            msg: 'An error has occured'
         })
     }else {
         next()
