e2f20e5a6cdb11c2e162fbb48182c1650a063aac
Tarynleewalbrugh25 <146818417+Tarynleewalbrugh25@users.noreply.github.com>
2024-08-13 13:28:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next)=>{
     next();
 })
 app.use(
-    express.static('./static'),
+    express.static('./Static'),
     express.json(),
     express.urlencoded({
         extended: true,
@@ -28,7 +28,7 @@ app.use(
     cors()
 )
 app.get('^/$|/Crimewatch', (req, res) => {
-    res.status(200).sendFile(path.resolve('./static/index.html'))
+    res.status(200).sendFile(path.resolve('./Static/index.html'))
 })
 // app.use('/^')
 // app.use('^/$')
